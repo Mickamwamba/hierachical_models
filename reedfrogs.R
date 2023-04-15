cbb5b4c5219597142455b728fbb7facc6207d8f9
mickamwamba <kimollomick17@gmail.com>
2023-04-15 09:42:25 -0400

    feat: add predictors to the model

--- reedfrogs.R
+++ reedfrogs.R
@@ -14,11 +14,38 @@ print(model1,pars = c('alpha'))
 
 #considering each tank separately - fitting with no pooling
 model2 <- stan('reedfrogs_no_pooling.stan',data=stan_data,cores=4)
-print(model1,pars=c('alpha'))
+print(model2,pars=c('alpha'))
 
 #considering shared information between group - fitting partial pooling / hierachical. 
 model3 <- stan('reedfrogs_partial_pooling.stan',data=stan_data,cores=4)
 print(model3,pars=c('alpha','mu','tau'))
+
+library(loo)
+
+loo1= loo(model1)
+loo2= loo(model2)
+loo3= loo(model3)
+
+loo_compare(loo1,loo2,loo3)
+loo_model_weights(list(loo1,loo2,loo3))
+
+# Add Predictors: 
+
+predictors <- model.matrix(~pred+size,data=reedfrogs)
+#Define the grouping structure: 
+total_tanks <- length(survivors); #each data is from one tank
+tank_tags <- seq(1,total_tanks)
+
+stan_data2 <- list(N=length(survivors),K=ncol(predictors),predictors=predictors,survivors=survivors,
+                   sample_sizes=sample_sizes,total_tanks=total_tanks,tank_tags=tank_tags)
+
+model4 <- stan('reedfrogs_partial_pooling_predictors.stan',data=stan_data2,cores=2)
+print(model4,pars=c('alpha','mu','tau'))
+
+#Compare this model with the rest
+loo4= loo(model4)
+loo_compare(loo1,loo2,loo3,loo4)
+
 
 
 
--- reedfrogs_partial_pooling_predictors.stan
+++ reedfrogs_partial_pooling_predictors.stan
@@ -1,7 +1,11 @@
 data {
   int<lower=0> N;
+  int<lower=0> K;
+  int total_tanks;
+  int tank_tags [total_tanks];
   int survivors [N]; 
   int sample_sizes [N];
+  matrix [N,K] predictors;
 }
 
 // The parameters accepted by the model. Our model
@@ -10,6 +14,7 @@ parameters {
   vector[N] alpha_tilde;
   real mu;
   real <lower=0> tau; // from the parent process
+  vector [K] beta;
 }
 
 transformed parameters{
@@ -21,12 +26,13 @@ model {
   mu ~ normal(0,1);
   tau ~ cauchy(0,1);
   alpha_tilde ~std_normal();
-  survivors ~ binomial_logit(sample_sizes,alpha);
+  for (i in 1:N) 
+      survivors[i] ~ binomial_logit(sample_sizes[i],predictors[i]*beta+alpha[tank_tags[i]]);
 }
 
 generated quantities{
   vector [N] log_lik;
-  for (i in 1:N) log_lik[i] = binomial_logit_lpmf(survivors[i]|sample_sizes[i],alpha[i]);
+  for (i in 1:N) log_lik[i] = binomial_logit_lpmf(survivors[i]|sample_sizes[i],predictors[i]*beta+alpha[tank_tags[i]]);
 }
 
 
